42dd9c24ce27463bed5a54839a19d83ac327cf7f
eospgonz10 <estiven.ospinag@udea.edu.co>
2025-11-18 17:03:02 -0500

    Se publica base del proyecto

---
+++ src/main/java/com/udea/demonmap/DemonmapApplication.java
@@ -0,0 +1,13 @@
+package com.udea.demonmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/demonmap/DemonmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.demonmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
